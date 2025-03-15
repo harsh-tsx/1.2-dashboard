85acfd3d3663c420e2abf16780703188a963f498
harsh-tsx <probookh118@gmail.com>
2025-03-15 10:48:19 +0530

    bug fixes

--- apps/isomorphic/src/validators/plant.schema.ts
+++ apps/isomorphic/src/validators/plant.schema.ts
@@ -1,8 +1,8 @@
 import { array, number, object, string } from "yup";
 
 export const plantSchema = object().shape({
-    name: string().label("Plant Name").required(),
-    address: string().label("Plant Address").required(),
+    name: string().trim().label("Plant Name").required(),
+    address: string().trim().label("Plant Address").required(),
     lat: number().label("Latitude").meta({ className: "w-full" }).required(),
     long: number().label("Longitude").meta({ className: "w-full" }).required(),
 }).label("Add").meta({ button: "Create" })
